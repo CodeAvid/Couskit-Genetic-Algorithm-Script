be4b4e878f5b7ff08c5a7bf34cacb37f63cb63f4
lordghostx <solomonghost9@gmail.com>
2020-11-05 17:02:01 +0100

    added data validation

--- app.py
+++ app.py
@@ -26,7 +26,6 @@ def timetable_callback(timetable_data, api_url="https://tbe-node-deploy.herokuap
     timetable = format_timetable(timetable)
     r = requests.get(api_url, json=timetable, headers={
         "Content-Type": "application/json"})
-    print(timetable)
 
 
 @app.route("/")
@@ -37,9 +36,31 @@ def index():
 @app.route("/generate/")
 def generate():
     timetable_data = request.get_json()
+    if timetable_data.get("Classrooms") == None:
+        return {"success": False, "message": "Classrooms variable is missing from the input"}, 400
+    else:
+        if not isinstance(timetable_data["Classrooms"], dict):
+            return {"success": False, "message": "Classrooms variable must be a dictionary"}, 400
+        for i in timetable_data["Classrooms"]:
+            if not isinstance(timetable_data["Classrooms"][i], list):
+                return {"success": False, "message": "each key in Classrooms dictionary must be a list"}, 400
+
+    if timetable_data.get("Classes") == None:
+        return {"success": False, "message": "Classes variable is missing from the input"}, 400
+    else:
+        if not isinstance(timetable_data["Classes"], list):
+            return {"success": False, "message": "Classes variable must be a list"}, 400
+        for i in timetable_data["Classes"]:
+            if not isinstance(i, dict):
+                return {"success": False, "message": "each element in Classes list must be a dictionary"}, 400
+            if i.get("Subject") == None or i.get("Type") == None or i.get("Professor") == None or i.get("Groups") == None or i.get("AllowedClassrooms") == None or i.get("Length") == None:
+                return {"success": False, "message": "each element in Classes list must be a dictionary containing all of Subject, Type, Professor, Groups, AllowedClassrooms and Length"}, 400
+            if not isinstance(i["Groups"], list):
+                return {"success": False, "message": "Groups variable in Classes dictionary must be a list"}, 400
+
     thread = threading.Thread(target=timetable_callback, args=[timetable_data])
     thread.start()
-    return {"success": True}, 202
+    return {"success": True, "message": "the timetable is being generated"}, 202
 
 
 if __name__ == "__main__":
