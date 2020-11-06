ef6ea5367344a85e942e2b2fc2d065e4b5c4d2e8
lordghostx <solomonghost9@gmail.com>
2020-11-06 22:54:39 +0100

    updated data validation

--- app.py
+++ app.py
@@ -41,27 +41,40 @@ def generate():
             raise Exception()
     except:
         return {"success": False, "message": "the timetable data is missing"}, 400
-    if timetable_data.get("Classrooms") == None:
-        return {"success": False, "message": "Classrooms variable is missing from the input"}, 400
-    else:
-        if not isinstance(timetable_data["Classrooms"], dict):
-            return {"success": False, "message": "Classrooms variable must be a dictionary"}, 400
-        for i in timetable_data["Classrooms"]:
-            if not isinstance(timetable_data["Classrooms"][i], list):
-                return {"success": False, "message": "each key in Classrooms dictionary must be a list"}, 400
-
-    if timetable_data.get("Classes") == None:
-        return {"success": False, "message": "Classes variable is missing from the input"}, 400
-    else:
-        if not isinstance(timetable_data["Classes"], list):
-            return {"success": False, "message": "Classes variable must be a list"}, 400
-        for i in timetable_data["Classes"]:
-            if not isinstance(i, dict):
-                return {"success": False, "message": "each element in Classes list must be a dictionary"}, 400
-            if i.get("Subject") == None or i.get("Type") == None or i.get("Professor") == None or i.get("Groups") == None or i.get("AllowedClassrooms") == None or i.get("Length") == None:
-                return {"success": False, "message": "each element in Classes list must be a dictionary containing all of Subject, Type, Professor, Groups, AllowedClassrooms and Length"}, 400
-            if not isinstance(i["Groups"], list):
-                return {"success": False, "message": "Groups variable in Classes dictionary must be a list"}, 400
+    valid = True
+    if None in [timetable_data.get("classroom"), timetable_data.get("courses"), timetable_data.get("selectedDay")]:
+        valid = False
+    if valid and (not isinstance(timetable_data.get("classroom"), dict)):
+        valid = False
+    if valid and (not isinstance(timetable_data.get("courses"), list)):
+        valid = False
+    if valid and (not isinstance(timetable_data.get("selectedDay"), list)):
+        valid = False
+    if valid:
+        for i in timetable_data.get("classroom"):
+            j = timetable_data["classroom"][i]
+            if None in [j.get("type"), j.get("capacity")]:
+                valid = False
+                break
+            if (not isinstance(j.get("type"), str)) or (not isinstance(j.get("capacity"), int)):
+                valid = False
+                break
+            if j.get("type") not in ["theory", "lab"]:
+                valid = False
+                break
+    if valid:
+        for i in timetable_data.get("courses"):
+            if None in [i.get("name"), i.get("lecturer"), i.get("type"), i.get("students"), i.get("unit")]:
+                valid = False
+                break
+            if (not isinstance(i.get("name"), str)) or (not isinstance(i.get("lecturer"), str)) or (not isinstance(i.get("type"), str)) or (not isinstance(i.get("students"), int)) or (not isinstance(i.get("unit"), int)):
+                valid = False
+                break
+            if i.get("type") not in ["theory", "lab"]:
+                valid = False
+                break
+    if not valid:
+        return {"success": False, "message": "the timetable data is not correctly formatted"}, 422
 
     thread = threading.Thread(target=timetable_callback, args=[timetable_data])
     thread.start()
