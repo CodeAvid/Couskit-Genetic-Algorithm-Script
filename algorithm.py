bbdbd093c6a8a3a1ba15e8aedb6dca9c2951bb0f
lordghostx <solomonghost9@gmail.com>
2020-11-07 20:54:30 +0100

    added timetable output

--- algorithm.py
+++ algorithm.py
@@ -5,45 +5,45 @@ import mutation
 from copy import deepcopy
 
 
-def evolutionary_algorithm(data, api_url, days, max_generations=5001, num_runs=1):
+def evolutionary_algorithm(data, api_url, days, timetable_id, max_generations=5001, num_runs=1):
     best_timetable = None
     cost_function = cost_functions.cost
     cost_function2 = cost_functions.cost2
     neighbour = mutation.neighbour
 
     for i in range(num_runs):
-        chromosome = dt.generate_chromosome(data)
+        chromosome = dt.generate_chromosome(data, days=days)
         for j in range(max_generations):
-            new_chromosome = neighbour(deepcopy(chromosome))
-            ft = cost_function(chromosome)
+            new_chromosome = neighbour(deepcopy(chromosome), days=days)
+            ft = cost_function(chromosome, days=days)
             if ft == 0:
                 break
-            ftn = cost_function(new_chromosome)
+            ftn = cost_function(new_chromosome, days=days)
             if ftn <= ft:
                 chromosome = new_chromosome
             if j % 200 == 0:
                 # print('Iteration', j, 'cost', cost_function(chromosome))
                 requests.get(api_url, params={
-                             "current_progress": j // 2, "total_progress": max_generations - 1})
+                             "current_progress": j // 2, "total_progress": max_generations - 1, "Timetable-id": timetable_id})
 
         # print('Run', i + 1, 'cost', cost_function(chromosome), 'chromosome', chromosome)
         requests.get(api_url, params={
-                     "current_progress": max_generations // 2, "total_progress": max_generations - 1})
-        if best_timetable is None or cost_function2(chromosome) <= cost_function2(best_timetable):
+                     "current_progress": max_generations // 2, "total_progress": max_generations - 1, "Timetable-id": timetable_id})
+        if best_timetable is None or cost_function2(chromosome, days=days) <= cost_function2(best_timetable, days=days):
             best_timetable = deepcopy(chromosome)
 
     chromosome = best_timetable
     neighbour2 = mutation.neighbour2
     for j in range(3 * max_generations):
-        new_chromosome = neighbour2(deepcopy(chromosome))
-        ft = cost_function2(chromosome)
-        ftn = cost_function2(new_chromosome)
+        new_chromosome = neighbour2(deepcopy(chromosome), days=days)
+        ft = cost_function2(chromosome, days=days)
+        ftn = cost_function2(new_chromosome, days=days)
         if ftn <= ft:
             chromosome = new_chromosome
         if j % 200 == 0:
             # print('Iteration', j, 'cost', cost_function2(chromosome))
             requests.get(api_url, params={"current_progress": (
-                j // 6) + (max_generations // 2), "total_progress": max_generations - 1})
+                j // 6) + (max_generations // 2), "total_progress": max_generations - 1, "Timetable-id": timetable_id})
 
     # print('Run', 'cost', cost_function2(chromosome), 'chromosome', chromosome)
     # dt.write_data(chromosome[0], output_file)
--- app.py
+++ app.py
@@ -53,10 +53,13 @@ def preformat_timetable(timetable):
 def timetable_callback(timetable_data, api_url="https://tbe-node-deploy.herokuapp.com/timetable"):
     days = timetable_data["selectedDay"]
     timetable = preformat_timetable(timetable_data)
-    timetable = evolutionary_algorithm(timetable, api_url, days=days)
-    timetable = format_timetable(timetable)
-    r = requests.get(api_url, json=timetable, headers={
-                     "Content-Type": "application/json"}, params={"current_progress": 5000, "total_progress": 5000})
+    timetable = evolutionary_algorithm(timetable, api_url, days=days, timetable_data["Timetable-id"])
+    timetable = format_timetable(timetable, days=days)
+    timetable["timetable-name"] = timetable_data["timetable-name"]
+    timetable["academic-section"] = timetable_data["academic-section"]
+    timetable["Timetable-id"] = timetable_data["Timetable-id"]
+    r = requests.get(api_url, json=timetable, headers={"Content-Type": "application/json"}, params={
+                     "current_progress": 5000, "total_progress": 5000, "Timetable-id": timetable_data["Timetable-id"]})
 
 
 @app.route("/")
